84c5088e0082cebda9a2ca8533bf3ae04231a162
Himanshu Abburu <himanshuabburu@gmail.com>
2021-12-31 11:31:10 +0000

    testing purpose only

---
+++ src/dataFordhTesting.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/996329/pexels-photo-996329.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "Clothes",
+    location: "New York",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/women-clothes-hanging-on-hangers-clothing-rails-fashion-design-picture-id916092484?k=20&m=916092484&s=612x612&w=0&h=2aTLAucj_-qbbfhBiJEXfdiY3-k0gx0el8OrKFpi3O8=",
+    title: "Only the hanger",
+    location: "California",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/women-clothes-hanging-on-hangers-clothing-rails-fashion-design-picture-id916092484?k=20&m=916092484&s=612x612&w=0&h=2aTLAucj_-qbbfhBiJEXfdiY3-k0gx0el8OrKFpi3O8=",
+    title: "Only the hanger",
+    location: "California",
+  },
+  {
+    id: 4,
+    img: "https://media.istockphoto.com/photos/women-clothes-hanging-on-hangers-clothing-rails-fashion-design-picture-id916092484?k=20&m=916092484&s=612x612&w=0&h=2aTLAucj_-qbbfhBiJEXfdiY3-k0gx0el8OrKFpi3O8=",
+    title: "Only the hanger",
+    location: "California",
+  },
+];
