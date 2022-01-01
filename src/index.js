41b100098a6b95b646cd0393ec5f4f29e9abdd78
Himanshu Abburu <himanshuabburu@gmail.com>
2022-01-01 17:32:24 +0000

    Update to add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CharityHomePage from "./CharityHomePage";
+import CharityRegistration from "./CharityRegistration";
+import DonorHomepage from "./DonorHomepage";
+import DonorRegistraion from "./DonorRegistraion";
 import Homepage from "./Homepage";
 import Login from "./Login";
 
 const Pages = () => {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route
+          exact
+          path="/charityregistration"
+          element={<CharityRegistration />}
+        />
+
+        <Route exact path="/DonorRegistraion" element={<DonorRegistraion />} />
+
+        <Route exact path="/Login" element={<Login />} />
+
+        <Route exact path="/CharityHomePage" element={<CharityHomePage />} />
+
+        <Route exact path="/DonorHomepage" element={<DonorHomepage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 ReactDOM.render(
