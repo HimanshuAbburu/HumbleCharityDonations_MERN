f118747cc4c17cf0cf700672d6543e5ae1e97d4e
Himanshu Abburu <himanshuabburu@gmail.com>
2022-03-03 21:10:23 +0000

    Update to add login function

--- src/Login.js
+++ src/Login.js
@@ -1,33 +1,128 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Navigate, Link } from "react-router-dom";
 import Image from "./img/Login-Greetings.jpeg";
 import "./Login.css";
 
+import { signIn } from "./Authentication";
+
 const Login = () => {
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const [loginStatus, setLoginStatus] = useState({
+    status: false,
+    loading: false,
+    msg: "",
+    forgot: false,
+  });
+
+  const RedirectFromLogin = async (email, password) => {
+    await signIn(email, password).then((response) => {
+      setLoginStatus(response);
+      console.log(response);
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await RedirectFromLogin(email.current.value, password.current.value);
+  };
+
   return (
     <>
-      <h1 id="head">Login</h1>
-      <div className="main">
-        <div className="left">
-          <form className="login">
-            <div>
-              <label htmlFor="email" className="loginlabel">
-                Email
-              </label>
-              <input type="email" name="email" className="logininput" />
+      {loginStatus.status ? (
+        <RedirectPage LoginStatus={loginStatus} />
+      ) : (
+        <>
+          <div className="nav" id="nav">
+            <div className="navleft">
+              <p>H D</p>
             </div>
-            <div>
-              <label htmlFor="password" className="loginlabel">
-                Password
-              </label>
-              <input type="password" name="password" className="logininput" />
+            <div className="navright">
+              <Link to="/" className="button">
+                <p>Home</p>
+              </Link>
+
+              <Link to="/Login" className="button">
+                <p>Sign In</p>
+              </Link>
             </div>
-            <button className="loginsignin">Submit</button>
-          </form>
-        </div>
-        <div className="right">
-          <img src={Image} alt="" className="img" />
-        </div>
-      </div>
+          </div>
+          <h1 id="head">Login</h1>
+          <div className="main">
+            <div className="left">
+              <form className="login">
+                <div>
+                  <label htmlFor="email" className="loginlabel">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="logininput"
+                    ref={email}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="loginlabel">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="logininput"
+                    ref={password}
+                  />
+                </div>
+                <button
+                  className="loginsignin"
+                  type="submit"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </form>
+            </div>
+            <div className="right">
+              <img src={Image} alt="" className="img" />
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+const RedirectPage = ({ LoginStatus }) => {
+  return (
+    <>
+      {LoginStatus.userData.type === "charity" ? (
+        <Navigate
+          to="/CharityHomePage"
+          // state: {
+          //   uid: LoginStatus.uid,
+          //   name: LoginStatus.userData.Name,
+          //   address: LoginStatus.userData.Address,
+          //   city: LoginStatus.userData.City,
+          //   postcode: LoginStatus.userData.Postcode,
+          //   phoneNo: LoginStatus.userData.Phone,
+          //   email: LoginStatus.userData.Email,
+          // },
+        />
+      ) : (
+        <Navigate
+          to="/DonorHomepage"
+          // state: {
+          //   uid: LoginStatus.uid,
+          //   name: LoginStatus.userData.Name,
+          //   address: LoginStatus.userData.Address,
+          //   city: LoginStatus.userData.City,
+          //   postcode: LoginStatus.userData.Postcode,
+          //   phoneNo: LoginStatus.userData.Phone,
+          //   email: LoginStatus.userData.Email,
+          // },
+        />
+      )}
     </>
   );
 };
